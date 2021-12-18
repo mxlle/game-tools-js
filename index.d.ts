6360cf25e0d4a5536f08b017f99ae9882631bdef
mxlle <almut.m.f@gmail.com>
2021-12-18 17:48:53 +0100

    Update types

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,32 @@
 declare module 'game-tools-js' {
+    // colors
     export const allColors: string[];
+    // confetti
     export const triggerConfetti: (colors: string[], particleCount: number) => void;
     export const triggerColoredConfetti: (colors: string[], particleCount: number) => void;
     export const triggerEmojiConfetti: (emojis: string[], particleCount: number) => void;
+    // emoji util
     export const splitEmojis: (emojiString: string) => string[];
     export const isCharacterEmoji: (char: string) => boolean;
     export const isMovingEmoji: (emoji: string) => boolean;
+    // emoji sets
+    export const spaceCatEmojis: string[];
+    export const rainbowEmojis: string[];
+    export const christmasEmojis: string[];
+    export const setOf12: string[];
+    export const smileys: string;
+    export const catSmileys: string;
+    export const gestures: string;
+    export const bodyParts: string;
+    export const clothingAndAccessories: string;
+    export const animals: string;
+    export const nature: string;
+    export const weatherAndEarth: string;
+    export const foodAndDrink: string;
+    export const activityAndSport: string;
+    export const travelAndPlaces: string;
+    export const objects: string;
+    // random util
     export const shuffleArray: (array: any[]) => any[];
     export const randomInt: (max: number) => number;
 }
