784c41ae1e1f2ba45fdb85d94c0e332606892205
mxlle <almut.m.f@gmail.com>
2021-12-18 17:56:43 +0100

    Adapt index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-export * from 'src/colors/consts';
+export * from './src/colors/consts';
 
-export * from 'src/confetti/confetti';
-export * from 'src/confetti/emoji-confetti';
+export * from './src/confetti/confetti';
+export * from './src/confetti/emoji-confetti';
 
-export * from 'src/emojis/emoji-util';
-export * from 'src/emojis/sets';
+export * from './src/emojis/emoji-util';
+export * from './src/emojis/sets';
 
-export * from 'src/random-utils';
+export * from './src/random-utils';
